4d324e5809933d55b454d2a9fbc2c819c1f6c95b
Andrey Kovalenko <adrkov02@gmail.com>
2021-10-25 19:21:15 +0300

    create new view create form

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>All Tasks</h2>
+    <!-- Bootstrap шаблон... -->
+    <div class="panel-body">
+        <!-- Отображение ошибок проверки ввода -->
+    @include('common.errors')
+    <!-- Форма новой задачи -->
+        <form action="{{ url(route('tasks.store') }}" method="POST" class="form-horizontal">
+        {{ csrf_field() }}
+
+        <!-- Имя задачи -->
+            <div class="form-group">
+                <label for="task" class="col-sm-3 control-label">Задача</label>
+
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="task-name" class="form-control" id="task">
+                </div>
+            </div>
+
+            <!-- Кнопка добавления задачи -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> Добавить задачу
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <a href="{{route('tasks.create')}}" class="btn btn-success"><i class="fa fa-plus"> Create new task</i></a>
+
+    <!-- TODO: Текущие задачи -->
+@endsection
